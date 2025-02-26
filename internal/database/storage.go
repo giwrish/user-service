7c595d1106bdcedccc5eab30c7e039bef37446a0
giwrish <shinde.girish.r@gmail.com>
2025-02-26 23:12:03 +0100

    adding conn configs

--- internal/database/storage.go
+++ internal/database/storage.go
@@ -3,6 +3,7 @@ package database
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/giwrish/user-service/internal/config"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -13,6 +14,10 @@ func Connect(cfg *config.DatabaseConfig) *pgxpool.Pool {
 	pgConnStr := cfg.Url
 
 	dbConfig, err := pgxpool.ParseConfig(pgConnStr)
+
+	dbConfig.MaxConns = cfg.MaxConnection
+	dbConfig.MaxConnIdleTime = time.Duration(cfg.IdleConnections)
+	dbConfig.MaxConnLifetime = time.Duration(cfg.MaxConnectionLife)
 
 	if err != nil {
 		log.Fatalf("Could not parse database url: %v", err.Error())
